2ac5da103431f527fdb196511f6cb349191b7397
Yosuke Nishizawa <yosuke@funadanoMacBook-Air.local>
2021-11-09 10:57:56 +0900

    イベント詳細ページ実装

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,7 @@ module AwesomeEvents
     # in config/environments, which are processed later.
     #
     config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
     config.action_view.form_with_generates_remote_forms = true
   end
