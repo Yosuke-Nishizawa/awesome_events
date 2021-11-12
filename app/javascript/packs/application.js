8f35d0fe3d2678d7658ef36587df5e800c0c4494
Yosuke Nishizawa <yosuke@funadanoMacBook-Air.local>
2021-11-12 14:31:13 +0900

    elastic searchを使った検索機能追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "get_form_turbolinks"
 import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
 
